40d2aef683734bf53be37e43daaaceeb89cc31eb
萧上晋 <84136399+Shangjin-Xiao@users.noreply.github.com>
2021-07-10 16:54:15 +0800

    Add files via upload

--- Pong/Pong.py
+++ Pong/Pong.py
@@ -1,1 +1,83 @@
+import pygame, sys
+class Ball(pygame.sprite.Sprite):
+    def __init__(self, image_file, speed, location):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load(image_file)
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+        self.speed = speed
 
+    def move(self):
+        global score, score_surf, score_font
+        self.rect = self.rect.move(self.speed)
+        if self.rect.left < 0 or self.rect.right > screen.get_width():
+            self.speed[0] = -self.speed[0]
+
+        if self.rect.top <= 0 :
+            self.speed[1] = -self.speed[1]
+            score = score + 1
+            score_surf = score_font.render(str(score), 1, (0, 0, 0))
+
+class Paddle(pygame.sprite.Sprite):
+    def __init__(self, location = [0,0]):
+        pygame.sprite.Sprite.__init__(self)
+        image_surface = pygame.surface.Surface([100, 20])
+        image_surface.fill([0,0,0])
+        self.image = image_surface.convert()
+        self.rect = self.image.get_rect()
+        self.rect.left, self.rect.top = location
+
+pygame.init()
+screen = pygame.display.set_mode([640,480])
+clock = pygame.time.Clock()
+myBall = Ball('wackball.png', [10,5], [50, 50])
+ballGroup = pygame.sprite.Group(myBall)
+paddle = Paddle([270, 400])
+lives = 3
+score = 0
+score_font = pygame.font.SysFont('SimHei', 25)
+score_surf = score_font.render(str(score), 1, (0, 0, 0))
+BLACK=(0,0,0)
+score_pos = [10, 10]
+done = False
+running = True
+while running:  
+    clock.tick(30)
+    screen.fill([255, 255, 255])
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+        elif event.type == pygame.MOUSEMOTION:
+            paddle.rect.centerx = event.pos[0]
+    if pygame.sprite.spritecollide(paddle, ballGroup, False):
+        myBall.speed[1] = -myBall.speed[1]
+    myBall.move()  
+    if not done:
+        screen.blit(myBall.image, myBall.rect)
+        screen.blit(paddle.image, paddle.rect)
+        smdf="生命数 / Number of lives :" + str(lives) + "     " +"得分 / Score :" + str(score)
+        text=score_font.render(smdf,True,BLACK)
+        text_rect=text.get_rect()
+        text_rect.centerx=screen.get_rect().centerx
+        text_rect.y=10
+        screen.blit(text,text_rect)
+        pygame.display.flip()
+    if myBall.rect.top >= screen.get_rect().bottom:
+        lives = lives - 1
+        if lives == 0:
+            final_text1 = "Game Over / 游戏结束"
+            final_text2 = "Your final score is: / 你的最终成绩是：" + str(score) + "分"
+            ft1_font = pygame.font.SysFont('SimHei', 50)
+            ft1_surf = ft1_font.render(final_text1, 1, (0, 0, 0))
+            ft2_font = pygame.font.SysFont('SimHei', 30)
+            ft2_surf = ft2_font.render(final_text2, 1, (0, 0, 0))
+            screen.blit(ft1_surf, [screen.get_width()//2 - \
+                        ft1_surf.get_width()//2, 100])
+            screen.blit(ft2_surf, [screen.get_width()//2 - \
+                        ft2_surf.get_width()//2, 200])
+            pygame.display.flip()
+            done = True
+        else:
+            pygame.time.delay(2000)
+            myBall.rect.topleft = [50, 50]
+pygame.quit()
